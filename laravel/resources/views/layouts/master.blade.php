6a5799bbc1396a13cfc834acf9e7057fc589fdda
arthmael8610 <arthamel86@gmail.com>
2018-08-20 08:40:37 -0400

    BIG Push, lots of features org logo will display on back of event for easier identification.
    added timezone selection as well as ip and JS based.
    
    fixed edit/delete menu to show for creator.
    
    added extra event fields to edit method as well as create.
    
    small changes, made edit / delete dropdown click instead of hover
    added the mission brief url to view page if exists.
    
    changed up the ip detection to a new more reliable API.
    
    backend cleaning of code to stop duplication of code.
    
    changed date number color so that it can be determined on a day with an event.
    
    changed registration to get all info in one go instead of requiring 2 steps.
    
    Added all contractor orgs to a DB with logo's and RSI links for footer, and cross use on citizenwarfare.
    
    copied terms, and privacy policies from citizenwarfare.
    
    updated to font awesome 5.
    
    added about dev page.
    
    Added HotJar for tracking and feedback! - adjusted code to below js declaration
    
    changed avatar dimension restriction, set size restriction to 1gig
    
    Total overhaul of code, organizations now have there own private calendar, they can share this with other orgs org keep it private, they can also share specific events while keeping others under wraps.
    
    updated font to hopefully allow it to use the zebulon font online, and not just local
    
    minor tweek to organization migration
    
    stopped ignoring org_logos x2
    
    scope issue fixes x4
    
    added google analytics
    
    Signed-off-by: arthmael8610 <arthamel86@gmail.com>

--- laravel/resources/views/layouts/master.blade.php
+++ laravel/resources/views/layouts/master.blade.php
@@ -14,7 +14,15 @@
             a.appendChild(r);
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-98254273-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
+        gtag('config', 'UA-98254273-3');
+    </script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
