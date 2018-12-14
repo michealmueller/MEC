93f471124a31e9f1b1c7691433a2f73a73b97bf4
arthmael8610 <micheal@muellertek.com>
2018-12-14 11:33:08 -0500

    BIG Push, lots of features            org logo will display on back of event for easier identification.
               added timezone selection as well as ip and JS based.
    
               fixed edit/delete menu to show for creator.
    
               added extra event fields to edit method as well as create.
    
               small changes, made edit / delete dropdown click instead of hover
               added the mission brief url to view page if exists.
    
               changed up the ip detection to a new more reliable API.
    
               backend cleaning of code to stop duplication of code.
    
               changed date number color so that it can be determined on a day with an event.
    
               changed registration to get all info in one go instead of requiring 2 steps.
    
               Added all contractor orgs to a DB with logo's and RSI links for footer, and cross use on citizenwarfare.
    
               copied terms, and privacy policies from citizenwarfare.
    
               updated to font awesome 5.
    
               added about dev page.
    
               Added HotJar for tracking and feedback! - adjusted code to below js declaration
    
               changed avatar dimension restriction, set size restriction to 1gig
    
               Total overhaul of code, organizations now have there own private calendar, they can share this with other orgs org keep it private, they can also share specific events while keeping others under wraps.
    
               updated font to hopefully allow it to use the zebulon font online, and not just local
    
               minor tweek to organization migration
    
               stopped ignoring org_logos x2
    
               scope issue fixes x4
    
               added google analytics
    
               made calendars private by requiring an account and being logged in.
    
               other QOL changes
    
               Added Public Event ticker to bottom of viewport.
    
               fix for IP issue
    
               update to notifications to make them hang out for a sec
    
               small update to frontend.
    
               added admin section user/visitor tracking
    
               fixed issues with timezone if API dies (use ?timezone={your_timezone_name} )
    
               fixed organization request to show all requests, not just 1 at a time.
    
               Revamp of organization member registration
    
               Normalized HTML, enhanced backend code.
    
               simple typo
    
               fixed reference generation
    
               added template to lost password emails.
    
               Async with ajax for most profile updates.
    
               moved RSS feed updates to background cron job.
    
               testing live push notifications.
    
    Signed-off-by: arthmael8610 <micheal@muellertek.com>

---
+++ public_html/DiscordBot/bot.js
@@ -0,0 +1,85 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    // List servers the bot is connected to
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        var organizations = (" - " + guild.name);
+
+        // List all channels
+        guild.channels.forEach((channel) => {
+            var channels = ` -- ${channel.name} (${channel.type}) - ${channel.id}`
+        })
+    })
+})
+
+client.login("NTIyODg1NjcyMjQ4Mjc5MDQx.DvShZg.l2osvDJI-vSmnfOyShbIXf7WWFM") // Replace XXXXX with your bot token
+
+client.on('ready', () => {
+    var generalChannel = client.channels.get("123456789") // Replace with known channel ID
+    generalChannel.send("Hello, world!")
+})
+
+client.on('message', (receivedMessage) => {
+  if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+    return
+  }
+    // Check if the bot's user was tagged in the message
+    if (receivedMessage.content.includes(client.user.toString())) {
+        if (receivedMessage.content.startsWith("!")) {
+            processCommand(receivedMessage)
+        }
+    }
+})
+
+function processCommand(receivedMessage) {
+    let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments) // There may not be any arguments
+
+    if (primaryCommand == "help") {
+        helpCommand(arguments, receivedMessage)
+    }else if (primaryCommand == "setup") {
+        setupCommand(arguments, receivedMessage)
+    }else {
+        receivedMessage.channel.send("I don't understand the command. Try `!help``")
+    }
+}
+
+function helpCommand(arguments, receivedMessage) {
+    if (arguments.length > 0) {
+        receivedMessage.channel.send("It looks like you might need help with " + arguments)
+    } else {
+        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
+    }
+}
+
+function setupCommand(arguments, receivedMessage) {
+    receivedMessage.channel.send('Thank you for using the me, the Mec Bot, Your Event Assistant!!');
+    receivedMessage.channel.send('First thing we will do is get you a webhook, this way i can reliably recieve notifications and forward those to you.');
+    receivedMessage.channel.send('Setting up the webhook now.');
+//Create the webhook and take care of user error and spaces
+    const nameAvatar = args.join(" ");
+    const linkCheck = /https?:\/\/.+\.(?:png|jpg|jpeg)/gi;
+    if (!linkCheck.test(nameAvatar)) return message.reply("You must supply an image link.");
+    const avatar = nameAvatar.match(linkCheck)[0];
+    const name = nameAvatar.replace(linkCheck, "");
+
+    Webhook = receivedMessage.channel.createWebhook(name, avatar).then(wb => receivedMessage.channel.send(
+        'Here is you webhook link, https://discordapp.com/api/webhooks/${wb.id}/${wb.token}')).catch(console.error)
+        receivedMessage.channel.send('Webhook created successfully!')
+
+        receivedMessage.channel.send('there was an issue creating the WebHook, Contact my creator [Ω] Arthmael#9572 , and notify him of this please.')
+    }
+
+  if (arguments.length > 0) {
+        receivedMessage.channel.send("It looks like you might need help with " + arguments)
+    } else {
+        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
+    }
+}
