62e31eb09dc79d0d58067c4f185bda46aae1c148
Micheal Mueller <Arthmael8610@users.noreply.github.com>
2018-12-19 10:28:09 -0500

    Delete botSettings.js

--- public_html/DiscordBot/botSettings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    token : 'NTI0OTM2ODc1NjUyNDE1NDk5.DvvVTQ.yINN5fS6T0vOIBnLF3QYt_IGMLg',
-    prefix:'!!',
-    dbhost:"localhost",
-    dbuser:"admin_events",
-    dbpass:"RUBwrJiyoN",
-    dbdatabase:"admin_events"
-};
