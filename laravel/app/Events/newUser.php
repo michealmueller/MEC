a315a5c4950a68de88c5820255fa16930bdc7c81
arthmael8610 <micheal@muellertek.com>
2019-01-11 18:11:25 -0500

    Changes:visual overhaul, and some suttle changes, single users can now create and share events, fixed organization calendar not working, removed un-needed libraries to lighten up the code base.
    Signed-off-by: arthmael8610 <micheal@muellertek.com>

--- laravel/app/Events/newUser.php
+++ laravel/app/Events/newUser.php
@@ -2,8 +2,6 @@
 
 namespace App\Events;
 
-use App\Organization;
-use App\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
@@ -12,29 +10,26 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class newUser
+class NewUser
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $user;
-    public $organization;
-
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct(User $user)
+    public function __construct($user)
     {
         //
         $this->user = $user;
-
     }
 
     /**
      * Get the channels the event should broadcast on.
      *
-     * @return \Illuminate\Broadcasting\Channel|array
+     * @return Channel|array
      */
     public function broadcastOn()
     {
