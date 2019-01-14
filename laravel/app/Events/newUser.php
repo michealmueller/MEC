b46649776136f9b75c790771197fa5925b6da31e
arthmael8610 <micheal@muellertek.com>
2019-01-14 13:19:18 -0500

    Changes:updated SEO rules to hopefully get us higher on the SE listings
    Signed-off-by: arthmael8610 <micheal@muellertek.com>

--- laravel/app/Events/newUser.php
+++ laravel/app/Events/newUser.php
@@ -2,15 +2,11 @@
 
 namespace App\Events;
 
-use App\Organization;
 use App\User;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class newUser
 {
---
+++ laravel/config/seotools.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    'meta'      => [
+        /*
+         * The default configurations to be used by the meta generator.
+         */
+        'defaults'       => [
+            'title'        => "CitizenWarfare", // set false to total remove
+            'description'  => 'The premier event site for Star Citizen, We are trying to bring players and organizations together to create Co-operation and fun!', // set false to total remove
+            'separator'    => ' - ',
+            'keywords'     => ['Star Citizen', 'Events', 'Event Manager', 'Organizations', 'Around the Verse', 'Citizenwarfare', 'Community'],
+            'canonical'    => false, // Set null for using Url::current(), set false to total remove
+        ],
+
+        /*
+         * Webmaster tags are always added.
+         */
+        'webmaster_tags' => [
+            'google'    => null,
+            'bing'      => null,
+            'alexa'     => null,
+            'pinterest' => null,
+            'yandex'    => null,
+        ],
+    ],
+    'opengraph' => [
+        /*
+         * The default configurations to be used by the opengraph generator.
+         */
+        'defaults' => [
+            'title'       => 'CitizenWarfare', // set false to total remove
+            'description' => 'The premier event site for Star Citizen, We are trying to bring players and organizations together to create Co-operation and fun!', // set false to total remove
+            'url'         => null, // Set null for using Url::current(), set false to total remove
+            'type'        => null,
+            'site_name'   => 'CitizenWarfare',
+            'images'      => ['https://citizenwarfare.com/storage/app/logos/Citizen_Warfare_Logo_White.png'],
+        ],
+    ],
+    'twitter' => [
+        /*
+         * The default values to be used by the twitter cards generator.
+         */
+        'defaults' => [
+            'card'      => 'summary',
+            'title'     => 'CitizenWarfare - Home',
+            //'site'    => '@LuizVinicius73',
+            'creator'   => '@arthmael86',
+        ],
+    ],
+];
