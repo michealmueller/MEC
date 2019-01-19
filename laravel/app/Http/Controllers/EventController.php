13bb8de6aaa2987cce4e13348cc8d744dc7a0dd3
arthmael8610 <micheal@muellertek.com>
2019-01-19 18:38:27 -0500

    Changes:fix for bot push, finally
    Signed-off-by: arthmael8610 <micheal@muellertek.com>

--- laravel/app/Http/Controllers/EventController.php
+++ laravel/app/Http/Controllers/EventController.php
@@ -401,13 +401,23 @@ class EventController extends Controller
         $data = json_encode($data);
         $result = null;
 //clean object of null / undefined / empty strings.
-        foreach ($hooks as $k=>$v){
+        foreach ($hooks as $k => $v) {
+            //single user returns object adjust for that
+            if(!Auth::user()->organization){
+                $v = $v->public_webhook_url;
+            }
+
             if ($v == '' || $v == null || $v == 'undefined') {
                 unset($hooks[$k]);
             }
         }
-//continue on with push
-        foreach($hooks as $k=>$v) {
+        //continue on with push
+        foreach ($hooks as $k => $v) {
+            //single user returns object adjust for that
+            if(!Auth::user()->organization){
+                $v = $v->public_webhook_url;
+            }
+
             //dd($hooks, $k, $v);
             $ch = curl_init($v);
 
@@ -415,24 +425,24 @@ class EventController extends Controller
                 curl_setopt($ch, CURLOPT_POST, 1);
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-                curl_setopt($ch, CURLOPT_HEADER, array('Content-Type: application/json Content-Length: '.strlen($data)));
+                curl_setopt($ch, CURLOPT_HEADER, array('Content-Type: application/json Content-Length: ' . strlen($data)));
 
                 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
                 $result[$k] = curl_exec($ch);
             }
-            if(curl_getinfo($ch, CURLINFO_HTTP_CODE) != 204){
+            if (curl_getinfo($ch, CURLINFO_HTTP_CODE) != 204) {
                 //dd('If you see this please screen shot it and send it to Arthmael in discord or email it to support@citizenwarfare.com, Thank You.', $result, $hooks, $k, $v);
-                if(curl_error($ch)) {
+                if (curl_error($ch)) {
                     $result[$k]['error'] = curl_error($ch);
                 }
-                abort('404','There was an issue with Curl, I could not send the data to Discord, please contact support at support@citizenwarfare.com');
+                abort('404', 'There was an issue with Curl, I could not send the data to Discord, please contact support at support@citizenwarfare.com');
                 die;
             }
         }
         if ($result != null) {
             session()->put('success', 'Event has been pushed to CitizenWarfare Bot');
             //curl_close($ch);
-        }else{
+        } else {
             session()->put('info', 'Your event may have been created, but was not pushed to the bot.');
         }
     }
