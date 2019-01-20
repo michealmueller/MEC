727d9efdc942eea99a682f7d237e342dae718e60
arthmael8610 <micheal@muellertek.com>
2019-01-20 11:14:17 -0500

    Changes:attempting to get google adsense to work!
    Signed-off-by: arthmael8610 <micheal@muellertek.com>

--- laravel/resources/views/layouts/master.blade.php
+++ laravel/resources/views/layouts/master.blade.php
@@ -39,27 +39,6 @@ if ($two_letter_country_code == "US")
 <html lang="en">
 
 <head>
-    <!-- Hotjar Tracking Code for https://citizenwarfare.com -->
-    <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:983821,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-98254273-3"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-98254273-3');
-    </script>
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({
